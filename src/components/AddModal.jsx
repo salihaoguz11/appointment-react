0194519e134922809880f12a4b310b2c2dfe60d5
Saliha <salihaoguz2011@gmail.com>
2023-02-28 14:57:41 +0000

    addmodal

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 export function AddModal({ handleClose, show }) {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Appointment ...</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="namel">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="datetime">
+              <Form.Label>Day&Time</Form.Label>
+              <Form.Control type="datetime-local" placeholder="" />
+            </Form.Group>
+            <div className="text-center ">
+              <Button variant="success" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
