07e6984bf6c9d3fa8ef4d6a307868bd7691cbe51
Saliha <salihaoguz2011@gmail.com>
2023-02-28 14:48:15 +0000

    modal-setshow

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-export function AddModal() {
+export function AddModal({ handleClose, show }) {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,8 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { AddModal } from "./AddModal";
+import { useState } from "react";
 
 const Doctors = ({ doctors }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(true);
+  };
+
   return (
     <Container className="p-2">
       <h3 className="display-6  mb-3" style={{ color: "rgb(166, 18, 189)" }}>
@@ -15,12 +23,14 @@ const Doctors = ({ doctors }) => {
               src={dr.img}
               alt="dr.name"
               className="img-thumbnail doctor-img"
+              onClick={handleClick}
             />
-            <h4>{dr.name}</h4>
-            <h5>{dr.dep}</h5>
+            <h5>{dr.name}</h5>
+            <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
+      <AddModal show={show} handleClose={() => setShow(false)} />
     </Container>
   );
 };
