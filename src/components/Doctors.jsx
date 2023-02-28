93a531ea008bfb412858745545c51155ead58089
Saliha <salihaoguz2011@gmail.com>
2023-02-28 13:55:59 +0000

    doctors

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,5 +1,18 @@
-const Doctors = () => {
-  return <div></div>;
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Doctors = ({ doctors }) => {
+  return (
+    <Container className="p-2">
+      <h3 className="display-6 " style={{ color: "rgb(166, 18, 189)" }}>
+        Our Doctors
+      </h3>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Doctors;
