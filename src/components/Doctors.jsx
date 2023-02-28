8dcb0e9443aec3220b52303d2740a318514f70df
Saliha <salihaoguz2011@gmail.com>
2023-02-28 14:28:34 +0000

    app.css

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -5,13 +5,17 @@ import Col from "react-bootstrap/Col";
 const Doctors = ({ doctors }) => {
   return (
     <Container className="p-2">
-      <h3 className="display-6 " style={{ color: "rgb(166, 18, 189)" }}>
+      <h3 className="display-6  mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
-      <Row>
+      <Row className="justify-content-center">
         {doctors.map((dr) => (
-          <Col key={dr.id} sm={6} md={4} lg={3}>
-            <img src={dr.img} alt="dr.name" className="img-thumbnail" />
+          <Col key={dr.id} xs={6} sm={4} md={3}>
+            <img
+              src={dr.img}
+              alt="dr.name"
+              className="img-thumbnail doctor-img"
+            />
             <h4>{dr.name}</h4>
             <h5>{dr.dep}</h5>
           </Col>
