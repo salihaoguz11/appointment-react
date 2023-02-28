dcb156e93693241ece92ee7f8cd056a49377fa6f
Saliha <salihaoguz2011@gmail.com>
2023-02-28 13:49:14 +0000

    doctors data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Appointment from "../components/Appointment";
 import Doctors from "../components/Doctors";
-
+import { doctorData } from "../helper/data";
 const Home = () => {
+  const [doctors, setDoctors] = useState(doctorData);
+  console.log(doctors);
   return (
     <main className="text-center mt-2 vh-100">
       <h1 className="display-5 text-danger">SOUTHGATE HOSPITAL</h1>
-      <Doctors />
+      <Doctors doctors={doctors} />
       <Appointment />
     </main>
   );
