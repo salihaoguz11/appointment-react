afa07fd345eaa9669233044c5cb4cedc4e7ea845
Saliha <salihaoguz2011@gmail.com>
2023-03-01 14:32:55 +0000

    drName

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -3,12 +3,12 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 
-export function AddModal({ handleClose, show }) {
+export function AddModal({ handleClose, show, drName }) {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Appointment ...</Modal.Title>
+          <Modal.Title>Appointment for {drName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
