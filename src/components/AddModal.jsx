2aca689220b9cf67db7d08200525f5c4088adbec
Saliha <salihaoguz2011@gmail.com>
2023-03-01 14:45:09 +0000

    modal useStates

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -4,6 +4,14 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 
 export function AddModal({ handleClose, show, drName }) {
+  const [patientName, setPatientName] = useState("");
+  const [date, setDate] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    handleClose();
+  };
+
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -11,15 +19,22 @@ export function AddModal({ handleClose, show, drName }) {
           <Modal.Title>Appointment for {drName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="namel">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                onChange={(e) => setPatientName(e.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="datetime">
               <Form.Label>Day&Time</Form.Label>
-              <Form.Control type="datetime-local" placeholder="" />
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setDate(e.target.value)}
+              />
             </Form.Group>
             <div className="text-center ">
               <Button variant="success" type="submit" className="me-2">
